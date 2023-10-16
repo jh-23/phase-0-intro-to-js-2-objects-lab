424e2d844cd809701ee09e0bc55f3143fcd4ecea
John Heger <heger7@gmail.com>
2023-10-16 10:48:04 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const employee = {
-    name: 'Bill',
-    streetAddress: '11 Broadway',
+    name: "Sam",
+    streetAddress: "11 Broadway",
 };
+
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee};
-    newEmployee[key] = value;
-    return newEmployee;
-}
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+
+    return newEmployee
+};
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
-function deleteFromEmployeeByKey(employee,key) {
+
+function deleteFromEmployeeByKey(employee, key) {
     const newEmployee = {...employee};
     delete newEmployee[key];
     return newEmployee;
-}
+};
+
 function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
     employee[key] = value;
     return employee;
